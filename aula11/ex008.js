48a527a926f5bff7a807740ba830b7eed5d0559f
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-08-26 08:58:00 -0300

    Aulas JavaScript Condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do seu carro é de ${vel} KM/H`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('dirija sempre usando o cinto de segurança')
